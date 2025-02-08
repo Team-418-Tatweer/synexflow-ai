f2e03a7d33c609db427fbe04b10f01fc4f712ac6
akramoo <samirounis2@gmail.com>
2025-02-08 17:33:17 +0100

    organize repo

--- src/integration-plan/state_management/ProductPlanningState.py
+++
@@ -1,7 +0,0 @@
-from typing import Dict, Any
-from pydantic import BaseModel, Field
-
-class ProductPlanningState(BaseModel):
-    product_info: Dict[str, Any] = Field(default_factory=dict)
-    integrated_plan: str = ""
-    web_search_results: Dict[str, Any] = {}
